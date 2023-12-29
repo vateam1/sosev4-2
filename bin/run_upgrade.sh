ff3a441f41003e929d4cfba3efe3ffca0b4640a5
vateam1 <114690431+vateam1@users.noreply.github.com>
2023-12-29 17:53:27 -0500

    Update run_upgrade.sh

--- bin/run_upgrade.sh
+++ bin/run_upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash <(curl -H 'Cache-Control: no-cache' -sL --proto '=https' https://install-anthias.srly.io)
+bash <(curl -H 'Cache-Control: no-cache' -sL --proto '=https' https://raw.githubusercontent.com/vateam1/sosev4-2/main/bin/install.sh)
